cbfdc0df0527f33eae3e26bd56497db1e9c6944e
NemoTaek <vkfkdchfhr@gmail.com>
2023-11-21 19:57:46 +0900

    방 랜덤 생성 로직 개발
    방 랜덤 생성 로직 개발

--- Percentage/Assets/Script/Map.cs
+++ Percentage/Assets/Script/Map.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class Map : Singleton<Map>
 {
     [Header("----- Map Setting -----")]
+    int[] roomCount = { 0, 10, 15, 20, 25 };
+    public List<int> roomList;
+    public List<int> selectRoomArray;
+    public Room[] roomPrefabs;
     public Room[] rooms;
     public Room startRoom;
     public Room currentRoom;
@@ -20,22 +24,113 @@ public class Map : Singleton<Map>
     public int shopItemCount;
     public bool isItemSet;
 
-    void Start()
+    protected override void Awake()
     {
+        base.Awake();
+
+        roomList = new List<int>();
+        selectRoomArray = new List<int>();
+
         // 맵 정중앙은 41번
         mapPosition = 41;
         shopItemCount = 3;
         itemsInShop = new List<int>();
         itemPrefab = new Item[shopItemCount];
 
-        rooms = GetComponentsInChildren<Room>();
+        // 방 랜덤 배치
+        roomList.Add(41);
+        for (int i = 0; i < roomCount[GameManager.instance.stage]; i++)
+        {
+            selectRoomArray.Clear();
+            rooms = GetComponentsInChildren<Room>();
+            ArrangeMap();
+        }
+
+        // 맵 세팅
         mapSquare = GameManager.instance.ui.mapBoard.GetComponentsInChildren<Image>();
         MapInit();
+    }
+
+    void Start()
+    {
+        
     }
 
     void Update()
     {
         
+    }
+
+    void ArrangeMap()
+    {
+        // 위: -1, 아래: +1, 좌: +9, 우: -9
+        int nextMapPosition = roomList[roomList.Count - 1];
+        int upRoomIndex = nextMapPosition - 1;
+        int downRoomIndex = nextMapPosition + 1;
+        int leftRoomIndex = nextMapPosition + 9;
+        int rightRoomIndex = nextMapPosition - 9;
+        int weight = 0;
+
+        // 사방을 보면서 비어있으면(룸 리스트에 없다면) 해당 방향으로 가중치만큼 배열에 개수 추가
+        int isExistUpRoom = roomList.Find(x => x == upRoomIndex);
+        if (isExistUpRoom == 0)
+        {
+            weight = (nextMapPosition % 9) - 1;
+            for(int i=0; i<weight; i++)
+            {
+                selectRoomArray.Add(upRoomIndex);
+            }
+        }
+        int isExistDownRoom = roomList.Find(x => x == downRoomIndex);
+        if (isExistDownRoom == 0)
+        {
+            weight = 9 - (nextMapPosition % 9);
+            for (int i = 0; i < weight; i++)
+            {
+                selectRoomArray.Add(downRoomIndex);
+            }
+        }
+        int isExistLeftRoom = roomList.Find(x => x == leftRoomIndex);
+        if (isExistLeftRoom == 0)
+        {
+            weight = 8 - (nextMapPosition / 9);
+            for (int i = 0; i < weight; i++)
+            {
+                selectRoomArray.Add(leftRoomIndex);
+            }
+        }
+        int isExistRightRoom = roomList.Find(x => x == rightRoomIndex);
+        if (isExistRightRoom == 0)
+        {
+            weight = nextMapPosition / 9;
+            for (int i = 0; i < weight; i++)
+            {
+                selectRoomArray.Add(rightRoomIndex);
+            }
+        }
+
+        // 랜덤으로 설치할 방을 선택
+        Room randomRoom = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], transform);
+
+        // 위에서 추가한 방 인덱스 배열 중 랜덤으로 하나 선택
+        int selectRoomIndex = Random.Range(0, selectRoomArray.Count);
+        if (selectRoomArray[selectRoomIndex] == upRoomIndex)
+        {
+            randomRoom.transform.position = (rooms[rooms.Length - 1].transform.position + Vector3.up * 12);
+        }
+        else if (selectRoomArray[selectRoomIndex] == downRoomIndex)
+        {
+            randomRoom.transform.position = (rooms[rooms.Length - 1].transform.position + Vector3.down * 12);
+        }
+        else if (selectRoomArray[selectRoomIndex] == leftRoomIndex)
+        {
+            randomRoom.transform.position = (rooms[rooms.Length - 1].transform.position + Vector3.left * 20);
+        }
+        else if (selectRoomArray[selectRoomIndex] == rightRoomIndex)
+        {
+            randomRoom.transform.position = (rooms[rooms.Length - 1].transform.position + Vector3.right * 20);
+        }
+        roomList.Add(selectRoomArray[selectRoomIndex]);
     }
 
     public void MapInit()
